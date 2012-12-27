a2189cf5acefb1e878b0663f262222829e2c9b3a
andrie <apdevries@gmail.com>
2012-12-27 19:12:57 +0000

    Updated dropout test result to reflect changes in membersurvey

--- inst/tests/test-8-tools.R
+++ inst/tests/test-8-tools.R
@@ -6,7 +6,7 @@ context("tools")
 
 test_that("dropout calculation is correct", {
       
-      rest <- setNames(c(215, 213, 82), c("id", "Q35", "Q37"))
-      expect_equal(dropout(membersurvey[-(127:128)]), rest)
+      rest <- setNames(c(215, 35), c("id", "Q23_other"))
+      expect_equal(dropout(membersurvey[-(108:109)]), rest)
     })
 
